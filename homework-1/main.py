f8ff6a51850a08d7675db82a0f030fade6fafe3c
Anton Vedischev <vedishchevanton@gmail.com>
2023-04-20 02:37:25 +0300

    изменение в скрипте

--- homework-1/main.py
+++ homework-1/main.py
@@ -13,7 +13,7 @@ try:
         with conn.cursor() as cur:
             """Получение данных из таблицы employees_data.csv"""
             # Открывается файл employees_data.csv с помощью функции open().
-            with open('north_data/employees_data.csv', newline='') as csvfile:
+            with open('north_data/employees_data.csv', newline='', encoding='utf-8') as csvfile:
                 # Создается объект csv.reader() для чтения данных из файла CSV с заданными параметрами
                 reader = csv.reader(csvfile, delimiter=',', quotechar='"')
                 # С помощью функции next() пропускается первая строка файла CSV, содержащая заголовки столбцов.
@@ -24,18 +24,18 @@ try:
                     cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s)", row)
 
             """Получение данных из таблицы customers_data.csv"""
-            with open('north_data/customers_data.csv', newline='') as csvfile:
+            with open('north_data/customers_data.csv', newline='', encoding='utf-8') as csvfile:
                 reader = csv.reader(csvfile, delimiter=',', quotechar='"')
                 next(reader)
                 for row in reader:
                     cur.execute("INSERT INTO employees VALUES (%s, %s, %s)", row)
 
             """Получение данных из таблицы orders_data.csv"""
-            with open('north_data/orders_data.csv', newline='') as csvfile:
+            with open('north_data/orders_data.csv', newline='', encoding='utf-8') as csvfile:
                 reader = csv.reader(csvfile, delimiter=',', quotechar='"')
                 next(reader)
                 for row in reader:
-                    cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s)", row)
+                    cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s)", row)
 finally:
     # Закрывается курсор с помощью метода cur.close().
     # Закрывается соединение с базой данных с помощью метода conn.close()
