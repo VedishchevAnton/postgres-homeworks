746f11f7cb6580e3e16966ffc92c966bdf9c00f8
Anton Vedischev <vedishchevanton@gmail.com>
2023-04-20 02:13:43 +0300

    добавление и изменение sql команд

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE employees
 	first_name varchar(30) NOT NULL,
 	last_name varchar(30) NOT NULL,
 	title varchar(100) NOT NULL,
-	birth_date date NOT NULL,
+	birth_date date,
 	notes text
 );
 
